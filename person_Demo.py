406b437f0cc7233c7fa803a42c8aa8bccd70739a
Janarthanan-Gnanamurthy <janagnanam12@gmail.com>
2024-06-20 23:14:52 +0530

    misc

--- person_Demo.py
+++ person_Demo.py
@@ -50,13 +50,11 @@ while True:
     # Detect persons in the frame
     frame = detect_person(frame)
 
-    # Display the resulting frame
     cv2.imshow('Person Detection', frame)
 
     # Break the loop on 'q' key press
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the webcam and close windows
 cap.release()
 cv2.destroyAllWindows()
